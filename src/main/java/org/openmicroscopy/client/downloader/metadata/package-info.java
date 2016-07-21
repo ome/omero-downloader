619f1dcbe7f22457d5c963426737c1d4eb7ef550
Mark Carroll <M.T.B.Carroll@Dundee.ac.uk>
2016-07-21 13:28:27 +0100

    extract ROI methods from OmeroMetadata for local XML export
    as this is largely copy-and-paste, made best guess
    from OMERO source regarding authorship, copyright

---
+++ src/main/java/org/openmicroscopy/client/downloader/metadata/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package's classes organize by OME-XML schema top-level model objects
+ * the methods that one would normally find in {@link ome.services.blitz.impl.OmeroMetadata}.
+ */
+
+package org.openmicroscopy.client.downloader.metadata;

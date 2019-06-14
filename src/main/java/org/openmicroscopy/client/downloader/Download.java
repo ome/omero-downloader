7c5bc87e18dec3dc7eea0c291f1f238dfdc85573
Jean-Marie Burel <j.burel@dundee.ac.uk>
2019-06-14 14:41:59 +0100

    update import

--- src/main/java/org/openmicroscopy/client/downloader/Download.java
+++ src/main/java/org/openmicroscopy/client/downloader/Download.java
@@ -98,7 +98,7 @@ import omero.model.Roi;
 import omero.model.Screen;
 import omero.sys.ParametersI;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import org.openmicroscopy.client.downloader.options.OptionParser;
